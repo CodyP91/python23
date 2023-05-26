3fd13ba0c3e208847396c941de9a32605f58a971
Cody <cjoel91@hotmail.com>
2023-05-25 19:26:12 -0600

    Updates on dbcreds.py

--- dbcreds.py
+++ dbcreds.py
@@ -1,5 +1,21 @@
+import mariadb
+from dbcreds import host, port, user, password, database
+
 user = "root"
 password = "password"
 host = "localhost"
 port = 3306  
 database = "python2"
+cursor = conn.cursor()
+
+# Execute the stored procedure
+cursor.callproc("get_all_items")
+
+# Fetch the results
+results = cursor.fetchall()
+
+# Close the cursor and connection
+cursor.close()
+conn.close()
+
+# Print or process the results as needed
